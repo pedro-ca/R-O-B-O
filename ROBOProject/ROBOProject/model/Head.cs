bbf297079fa3b11fe0478c4bbcbe1afb2d8b2a74
Pedro Henric Rosário <50923316+pedro-ca@users.noreply.github.com>
2019-11-15 20:22:14 -0300

    Classes model + diagrama de classes

---
+++ ROBOProject/ROBOProject/model/Head.cs
@@ -0,0 +1,57 @@
+﻿namespace ROBOProject.model
+{
+    class Head
+    {
+        private int rotation;
+        private int inclination;
+
+
+
+        public Head(int rotation, int inclination)
+        {
+            this.rotation = rotation;
+            this.inclination = inclination;
+        }
+
+        public void updateRotation()
+        {
+            //code here
+        }
+        public void updateInclination()
+        {
+            //code here
+        }
+
+
+
+
+        //Getter e Setters
+        public int GetRotation()
+        {
+            return rotation;
+        }
+
+        public void SetRotation(int value)
+        {
+            rotation = value;
+        }
+
+        public int GetInclination()
+        {
+            return inclination;
+        }
+
+        public void SetInclination(int value)
+        {
+            inclination = value;
+        }
+
+
+
+        //toString
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
